cdeb5df20e838158c30a0e9c2db1ef0e7818d2ab
Juan Sebastian Vanegas <sebastian44van@gmail.com>
2017-10-26 19:55:16 -0500

    Primer Commit, se inicia con LinQ

---
+++ Clase2/Clase2/Persona.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase2
+{
+    public class Persona
+    {
+        /// <summary>
+        /// Referencia al identificador de la clase
+        /// </summary>
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public string Correo { get; set; }
+        public string Pais { get; set; }
+        public char Genero { get; set; }
+
+        /// <summary>
+        /// Incrementa el valor de la edad en 1
+        /// </summary>
+        public void aumentarEdad() {
+            this.Edad++;
+        }
+
+        /// <summary>
+        /// Reemplaza el valor de la propiedad pais
+        /// </summary>
+        /// <param name="pais">Referencia al nuevo pais</param>
+        public void cambiarNacionalidad(string pais) {
+            this.Pais = pais;
+        }
+
+
+    }
+}
---
+++ Clase2/Clase2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            var listaPersonas = new List<Persona>();
+            for (int i = 0; i < 100; i++)
+            {
+                listaPersonas.Add(new Persona()
+                {
+                    Id = i,
+                    Nombre = "Nombre " + i,
+                    Edad = random.Next(10, 80)
+                });
+            }
+
+            foreach (var item in listaPersonas)
+            {
+                Console.WriteLine(item.Nombre + "  Edad: " + item.Edad);
+            }
+
+            Console.WriteLine("Ahora ordenados por Edad");
+            var ordenadosPorEdad = from personitas in listaPersonas
+                                   where personitas.Edad < 50
+                                   orderby personitas.Edad ascending
+                                   select personitas;
+
+            foreach (var persona in ordenadosPorEdad)
+            {
+                Console.WriteLine(persona.Nombre + " -- Edad:" + persona.Edad);
+            }
+            Console.ReadKey();
+        }
+    }
+}
